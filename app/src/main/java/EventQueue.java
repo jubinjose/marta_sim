9cab612c4abc00219309ada63663bc08766c383d
jubinjose <jubin.jose@gmail.com>
2018-11-26 17:55:26 -0500

    Replaced EventQueue with a PriorityQueue implementation to help match output with provided solution files

--- app/src/main/java/EventQueue.java
+++ app/src/main/java/EventQueue.java
@@ -1,51 +1,38 @@
 import java.util.Comparator;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class EventQueue {
 
-    private ArrayList<Event> eventList = new ArrayList<Event>() {};
+    private Comparator<Event> eventComparator = Comparator.comparingInt(Event::getTime);
+    private PriorityQueue<Event> events = new PriorityQueue<Event>(eventComparator);
+
+    
 
     public void addEvent(Event event){
-        eventList.add(event);
+        events.add(event);
     }
 
     public Event popEventWithLowestTime(){
-        
-        // Break a tie (same logical time) with lowest bus number (for consistency across multiple runs)
-        Optional<Event> lowestTimeOptionalEvent = eventList.stream().min(Comparator.comparing(Event::getTime)
-                                                                                .thenComparing(Event::getObjectid)); 
-        
-        if (lowestTimeOptionalEvent.isPresent()){
-            Event event = lowestTimeOptionalEvent.get();
-            eventList.remove(event);
-            return event;
-        }
-        else{
-            return null;
-        }
+        return events.poll();
     }
 
     public void removeEventWithObjectId(int id){
-        
-        List<Event> matchingEventList = eventList.stream().filter(event -> event.getObjectid() == id).collect(Collectors.toList()); 
-
-        eventList.removeAll(matchingEventList); //Ideally we expect only one match because a ObjectId(BusId) won't be repeated in the eventQueue
+        //Ideally we expect only one match because a ObjectId(BusId) won't be repeated in the eventQueue
+        events.removeIf(event -> event.getObjectid() == id); 
 
     }
 
     @Override
     public String toString(){
 
-        List<Event> sortedList = eventList.stream()
-            .sorted(Comparator.comparing(Event::getTime))
-            .collect(Collectors.toList()); 
+        Iterator<Event> it = events.iterator();
 
         StringBuffer sb = new StringBuffer();
-        
-        for (Event event : sortedList) {
+
+        while (it.hasNext()) {
+            Event event = it.next();
             sb.append(String.format("%s , %s , %d%n", event.getTime() , event.getType() , event.getObjectid()));
-        }
+         }
 
         return sb.toString();
     }
