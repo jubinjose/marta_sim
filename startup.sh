88e6d72508e45993d8537bfa7e6a5f33ededb6a8
jubin <jjose38@gatech.edu>
2018-12-02 23:05:45 -0500

    Updated readme Deleted startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-## Please remember to kill the process manually after running using ps -ef 
-kill $(ps aux | grep '[S]imulationService' | awk '{print $2}')
-cd /home/student/git/cs6310/app
-java -cp "./target/classes:./target/classes/lib/*" SimulationService "../test/test0_instruction_demo.txt" "../test/probabilities/test_evening_distibution.csv" &
-x-www-browser http://localhost:4567/view.html 
-
-
